7ec991ab3339c850960d936e873e966eff8ca588
fadz72 <fadz1984@hotmail.com>
2016-09-28 16:31:54 +0800

    Render Images on Index Page

--- app/models/play.rb
+++ app/models/play.rb
@@ -2,6 +2,6 @@ class Play < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :category
 
-	  has_attached_file :play_img, styles: { medium: "250x350>", thumb: "325x475>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+  has_attached_file :play_img, :styles => { :play_index => "250x350>", thumb: "325x475>" }, default_url: "/images/:style/missing.png"
+	validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\z/
 end
